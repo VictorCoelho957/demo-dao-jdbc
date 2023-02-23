8f7c3b14f5c3d6f8b480ecf9628f94f1e1000d21
Perkles <victor.coelho@discente.ufma.br>
2023-02-23 18:17:55 -0300

    findAll implementions

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -95,9 +98,82 @@ public class SellerDaoJDBC  implements SellerDao{
 
 
 	@Override
+	//todos vendedores ordenados por nomes
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st= null;
+		ResultSet rs=null;
+		try {
+			st= conn.prepareStatement("SELECT seller.*,department.Name as DepName " 
+					+ "FROM seller INNER JOIN department " 
+					+"ON seller.DepartmentId = department.Id "
+					+"ORDER BY Name");
+			
+			rs=st.executeQuery();
+			//testar se veio resultado, caso contrari, resulta em nulo:
+			List<Seller> list= new ArrayList<>();
+			Map<Integer, Department> map= new HashMap<>();
+			while (rs.next()) {
+				//instancias de departamentos, verifica se o departamneto ja existe e, posteriormente o adciona como nulo
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if(dep==null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				//associação de departaemntos com vendodores 
+				Seller obj= instantionSeller(rs, dep);
+				list.add(obj);
+				}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+
+    //metodo de pesquisa por departamento
+	@Override
+	public List<Seller> finByDepartment(Department department) {
+		PreparedStatement st= null;
+		ResultSet rs=null;
+		try {
+			st= conn.prepareStatement("SELECT seller.*,department.Name as DepName " 
+					+ "FROM seller INNER JOIN department " 
+					+"ON seller.DepartmentId = department.Id "
+					+"WHERE DepartmentId = ? "
+					+"ORDER BY Name");
+			
+			st.setInt(1,department.getId());
+			rs=st.executeQuery();
+			//testar se veio resultado, caso contrari, resulta em nulo:
+			List<Seller> list= new ArrayList<>();
+			Map<Integer, Department> map= new HashMap<>();
+			while (rs.next()) {
+				//instancias de departamentos, verifica se o departamneto ja existe e, posteriormente o adciona como nulo
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if(dep==null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				//associação de departaemntos com vendodores 
+				Seller obj= instantionSeller(rs, dep);
+				list.add(obj);
+				}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	
 	
